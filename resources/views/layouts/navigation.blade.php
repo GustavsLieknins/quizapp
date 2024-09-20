a95f6f804afc02a72d83417155e4315637fb9f73
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-09-20 11:50:29 +0300

    123

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,14 +11,14 @@
         </div>
     </div>
     <div>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
 
-                    <x-nav-link :href="route('logout')" class="right-link page-link {{ request()->routeIs('logout') ? 'page-link-selected' : '' }}">
-                    <div class="right-link-div page-link-div {{ request()->routeIs('logout') ? 'page-link-selected-div' : '' }}">
-                        <button>Logout</button>
-                    </div>
-                    </x-nav-link>
-                </form>
+            <x-nav-link :href="route('logout')" class="right-link page-link {{ request()->routeIs('logout') ? 'page-link-selected' : '' }}">
+            <div class="right-link-div page-link-div {{ request()->routeIs('logout') ? 'page-link-selected-div' : '' }}">
+                <button>Logout</button>
+            </div>
+            </x-nav-link>
+        </form>
     </div>
 </nav>
