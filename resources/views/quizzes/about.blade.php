067d5993f32ec22c7b955d6755cf2d9bb9a41ea0
Marsels <ipb22.m.strikaitis@vtdt.edu.lv>
2024-09-30 08:52:53 +0200

    About update

--- resources/views/quizzes/about.blade.php
+++ resources/views/quizzes/about.blade.php
@@ -38,19 +38,19 @@
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
             <!-- Kristaps Section -->
             <div class="bg-gray-700 text-white shadow-lg rounded-lg p-6 text-center">
-                <h2 class="text-3xl font-bold mb-4">Master Designer</h2>
-                <h3 class="text-xl mb-4">Kristaps</h3>
-                <h2 class="text-lg mb-4">Kristaps ir talantīgs un izskatīgs dizainers, kurš spēj radīt vizuāli pievilcīgus un funkcionālus risinājumus. Viņa radošums un prasme izprast klientu vajadzības ļauj viņam izstrādāt dizainus, kas ne tikai izskatās lieliski, bet arī atbilst mērķiem. Kristaps vienmēr ir gatavs jauniem izaicinājumiem un spēj pielāgoties, lai nodrošinātu, ka katrs projekts ir veiksmīgs.</h2>
+                <h2 class="text-3xl font-bold mb-4">MEGA Designer</h2>
+                <h3 class="text-xl mb-4">Vasīlījs</h3>
+                <h2 class="text-lg mb-4">Vasīlījs ir talantīgs un LIELS dizainers, kurš spēj radīt vizuāli LIELUS un funkcionālus risinājumus. Viņa radošums un prasme izprast klientu vajadzības ļauj viņam izstrādāt dizainus, kas ne tikai izskatās LIELI, bet arī atbilst mērķiem. Vasīlījs vienmēr ir gatavs jauniem izaicinājumiem un spēj pielāgoties, lai nodrošinātu, ka katrs projekts ir LIELS.</h2>
                 <div class="flex justify-center">
-                    <img src="{{ asset('img/vasilijs.jpg') }}" class="w-32 h-32 rounded-full mb-4" alt="Kristaps' profile picture">
+                    <img src="{{ asset('img/vasilijs.jpg') }}" class="w-32 h-32 rounded-full mb-4" alt="Vasīlījs' profile picture">
                 </div>
             </div>
 
             <!-- Marsmells Section -->
             <div class="bg-gray-700 text-white shadow-lg rounded-lg p-6 text-center">
-                <h2 class="text-3xl font-bold mb-4">Special Teams</h2>
-                <h3 class="text-xl mb-4">Marsmelis</h3>
-                <h2 class="text-lg mb-4">Marsmelis ir īpašs komandas loceklis, kurš, neskatoties uz savām vajadzībām, vienmēr izrāda apbrīnojamu apņēmību un entuziasmu. Viņa unikālā perspektīva un pieredze bagātina mūsu darbu, un viņš spēj radīt inovatīvus risinājumus. Marsmells ir ļoti atbalstošs, vienmēr gatavs palīdzēt citiem, un viņa klātbūtne komandā veicina sapratni un iekļaušanu. Viņa centība iedvesmo visus, un mēs esam lepni, ka viņš ir daļa no mūsu grupas.</h2>
+                <h2 class="text-3xl font-bold mb-4">Special Consultant</h2>
+                <h3 class="text-xl mb-4">Marsels</h3>
+                <h2 class="text-lg mb-4">Marsels ir īpašs komandas loceklis, kurš, neskatoties uz visām grūtībam, vienmēr izrāda apbrīnojamu apņēmību un entuziasmu. Viņa unikālā perspektīva un pieredze bagātina mūsu darbu, un viņš spēj radīt inovatīvus risinājumus. Marsels ir ļoti atbalstošs, vienmēr gatavs palīdzēt citiem, un viņa klātbūtne komandā veicina sapratni un iekļaušanu. Viņa centība iedvesmo visus, un mēs esam lepni, ka viņš ir daļa no mūsu grupas.</h2>
                 <div class="flex justify-center">
                 <img src="{{ asset('img/marsels.jpg') }}" class="w-32 h-32 rounded-full mb-4" alt="Marsels' profile picture">
                 </div>
